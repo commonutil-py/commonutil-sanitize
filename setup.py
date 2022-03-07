7777f17dfe3c9b082323a072c3284bc677074430
Cheyin L <cheyinl@gmail.com>
2022-03-07 12:27:07 +0800

    add setup script

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+		name="commonutil-sanitize",
+		version="0.3.0",  # REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
+		description="Routines support text sanitization",
+		py_modules=[
+				"common_sanitize",
+		],
+		classifiers=[
+				"Development Status :: 5 - Production/Stable",
+				"Intended Audience :: Developers",
+				"License :: OSI Approved :: MIT License",
+				"Operating System :: POSIX",
+				"Programming Language :: Python :: 3.7",
+		],
+		license="MIT License",
+)
